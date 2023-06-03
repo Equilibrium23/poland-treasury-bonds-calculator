78aed48fd4f80887c0238c751edfe1b09c0ac61a
Equilibrium <wojciech.jedraski1999@gmail.com>
2023-06-04 01:29:05 +0200

    Refactor TOS calculator test

--- src/test/java/poland/treasury/bonds/calculator/tos/TosTreasuryBondCalculatorTest.java
+++ src/test/java/poland/treasury/bonds/calculator/tos/TosTreasuryBondCalculatorTest.java
@@ -10,9 +10,50 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.stream.Stream;
 
-import static poland.treasury.bonds.calculator.database.TOS.TOS0526;
+import static poland.treasury.bonds.calculator.database.TOS.*;
 
 class TosTreasuryBondCalculatorTest {
+    @ParameterizedTest
+    @MethodSource("tosSampleProspectusWithExpectedReturn")
+    public void testFullTimeBuyBack(TosProspectus tosProspectus, double expectedInvestmentReturn) {
+        //given
+        LocalDate firstDayOfBondRelease = tosProspectus.getSaleDateRange().startDate();
+        TosTreasuryBond tosTreasuryBond = TreasuryBondService.buyTosBonds(tosProspectus, 1, firstDayOfBondRelease);
+
+        //when
+        double investmentReturn = TosTreasuryBondCalculator.calculate(tosTreasuryBond, tosTreasuryBond.getDefaultBuyBackDay());
+
+        //then
+        Assertions.assertEquals(expectedInvestmentReturn, investmentReturn);
+    }
+
+    public static Stream<Arguments> tosSampleProspectusWithExpectedReturn() {
+        return Stream.of(
+                Arguments.of(TOS0825, 120.79),
+                Arguments.of(TOS0925, 120.79),
+                Arguments.of(TOS1025, 121.99),
+                Arguments.of(TOS1125, 121.99),
+                Arguments.of(TOS1225, 121.99),
+                Arguments.of(TOS0126, 121.99),
+                Arguments.of(TOS0226, 121.99),
+                Arguments.of(TOS0326, 121.99),
+                Arguments.of(TOS0426, 121.99),
+                Arguments.of(TOS0526, 121.99)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("tosEarlierBuyBack")
+    public void testEarlierBuyBack(LocalDate purchaseDay, LocalDate buyBackDate, double expectedInvestmentReturn) {
+        //given
+        TosTreasuryBond tosTreasuryBond = TreasuryBondService.buyTosBonds(TOS0526, 1, purchaseDay);
+
+        //when
+        double investmentReturn = TosTreasuryBondCalculator.calculate(tosTreasuryBond, buyBackDate);
+
+        //then
+        Assertions.assertEquals(expectedInvestmentReturn, investmentReturn);
+    }
 
     public static Stream<Arguments> tosEarlierBuyBack() {
         return Stream.of(
@@ -21,18 +62,10 @@ class TosTreasuryBondCalculatorTest {
                 Arguments.of(LocalDate.of(2023, Month.MAY, 1), LocalDate.of(2023, Month.JULY, 1), 101.14),
                 Arguments.of(LocalDate.of(2023, Month.MAY, 1), LocalDate.of(2023, Month.DECEMBER, 17), 104.30),
                 Arguments.of(LocalDate.of(2023, Month.MAY, 1), LocalDate.of(2024, Month.FEBRUARY, 2), 105.18),
-                Arguments.of(LocalDate.of(2023, Month.MAY, 1), LocalDate.of(2024, Month.JULY, 12 ), 108.29),
+                Arguments.of(LocalDate.of(2023, Month.MAY, 1), LocalDate.of(2024, Month.JULY, 12), 108.29),
                 Arguments.of(LocalDate.of(2023, Month.MAY, 1), LocalDate.of(2025, Month.MAY, 1), 114.17),
                 Arguments.of(LocalDate.of(2023, Month.MAY, 1), LocalDate.of(2026, Month.JANUARY, 29), 120.02),
                 Arguments.of(LocalDate.of(2023, Month.MAY, 1), LocalDate.of(2026, Month.MAY, 1), 121.99)
         );
-    }
-
-    @ParameterizedTest
-    @MethodSource("tosEarlierBuyBack")
-    public void testEarlierBuyBack(LocalDate purchaseDay, LocalDate buyBackDate, double expectedReturn){
-        TosTreasuryBond tosTreasuryBond = TreasuryBondService.buyTosBonds(TOS0526, 1, purchaseDay);
-
-        Assertions.assertEquals(expectedReturn, TosTreasuryBondCalculator.calculate(tosTreasuryBond, buyBackDate));
     }
 }
